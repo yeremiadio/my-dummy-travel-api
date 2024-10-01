8e4c21baec8e6ecb6a5f082e54c4a8cfdf44e6bd
yeremiadio <raikkonendio@gmail.com>
2024-10-01 20:20:51 +0700

    fix: remove admin panel

--- config/admin.ts
+++ config/admin.ts
@@ -14,5 +14,5 @@ export default ({ env }) => ({
     nps: env.bool('FLAG_NPS', true),
     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
   },
-  // serveAdminPanel: false,
+  serveAdminPanel: false,
 });
